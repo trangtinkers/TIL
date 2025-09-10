916c5b7d301ab0ca69f3c6a4254aa5f6558eb95a
Trang <trang@trangla.de>
2025-09-10 21:41:02 +0200

    lol forgot to save the file (#2)

--- racket/planning_flight.rkt
+++ racket/planning_flight.rkt
@@ -3,3 +3,14 @@
  (struct student (name id# dorm))
 
  (struct cities (name long lat))
+
+ (define neighbors (city)
+    "Find all cities within 1000 kilometers."
+    (find-all-if #'(lambda (c)
+            (and (not (eq c city))
+                (< (air-distance c city) 1000.0)))
+        *cities*))
+        
+(define city (name)
+    "Find the city with this name."
+    (assoc name *cities*))
