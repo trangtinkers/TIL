bcaf8eee7fae37346104cc1c8a6cbb15891c9a24
tinkererthinker <trang@trangla.de>
2025-09-30 22:25:59 +0200

    bug fix #2

--- racket/planning_flight.rkt
+++ racket/planning_flight.rkt
@@ -4,8 +4,9 @@
 
  (struct cities (name long lat))
 
- (define neighbors (city)
-    "Find all cities within 1000 kilometers."
+; this code is still written in lisp
+ (define (neighbors city)
+    ;Find all cities within 1000 kilometers.
     (find-all-if #'(lambda (c)
             (and (not (eq c city))
                 (< (air-distance c city) 1000.0)))
@@ -14,3 +15,10 @@
 (define city (name)
     "Find the city with this name."
     (assoc name *cities*))
+
+;; calculating air-distance
+
+(define (air-distance city1 city2)
+        ...)
+(define (distance point1 point2)
+        ...)
--- racket/structure_basics.rkt
+++ racket/structure_basics.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket  
 
 (struct student (name id# dorm))
 
