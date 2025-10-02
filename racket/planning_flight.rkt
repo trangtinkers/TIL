66740e476ec2665280d5d3e5865dabf8bc9decf1
Trang <trang@trangla.de>
2025-10-02 12:24:12 +0200

    added cities

--- racket/planning_flight.rkt
+++ racket/planning_flight.rkt
@@ -2,11 +2,11 @@
 
  (struct city (name long lat))
 
- (define cities)
+ (define cities 
+        (Atlanta 84.23 33.45))
 
-; this code is still written in lisp
- (define (neighbors city)
-    ;Find all cities within 1000 kilometers.
+ (define neighbors (city)
+    "Find all cities within 1000 kilometers."
     (find-all-if #'(lambda (c)
             (and (not (eq c city))
                 (< (air-distance c city) 1000.0)))
@@ -15,10 +15,3 @@
 (define city (name)
     "Find the city with this name."
     (assoc name *cities*))
-
-;; calculating air-distance
-
-(define (air-distance city1 city2)
-        ...)
-(define (distance point1 point2)
-        ...)
