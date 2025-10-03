6db70c02c94056a0b8769a4badfd4920d7736da8
Trazy <trang@trangla.de>
2025-10-03 21:39:45 +0200

    sdafs

--- racket/planning_flight.rkt
+++ racket/planning_flight.rkt
@@ -4,9 +4,8 @@
 
  (struct cities (name long lat))
 
-; this code is still written in lisp
- (define (neighbors city)
-    ;Find all cities within 1000 kilometers.
+ (define neighbors (city)
+    "Find all cities within 1000 kilometers."
     (find-all-if #'(lambda (c)
             (and (not (eq c city))
                 (< (air-distance c city) 1000.0)))
@@ -16,9 +15,3 @@
     "Find the city with this name."
     (assoc name *cities*))
 
-;; calculating air-distance
-
-(define (air-distance city1 city2)
-        ...)
-(define (distance point1 point2)
-        ...)
